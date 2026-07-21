e5adb14b09589f11b0dd187eed3213633386538f
Fabianqc <fabian05demayo@gmail.com>
2026-07-21 00:33:03 -0400

    Change backend port to 8003 in ecosystem.config.js

--- Backend/ecosystem.config.js
+++ Backend/ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "gdd-t2-backend",
       cwd: "./",
       script: "venv/bin/uvicorn",
-      args: "main:app --host 0.0.0.0 --port 8000 --workers 2",
+      args: "main:app --host 0.0.0.0 --port 8003 --workers 2",
       interpreter: "none",
       env: {
         NODE_ENV: "production"
